488b6ade24a4a33ac3e519c18115ca36ef52d58e
Kevin Pulido Predator <kevinp8001@gmail.com>
2020-03-30 21:36:57 -0500

    Changes...

--- k_util.py
+++ k_util.py
@@ -1,13 +1,5 @@
 def describe(element):
     try:
-        return print('Type:',type(element),'| Size:', element.shape)
+        return print('Type:',type(element),'|| Size:', element.shape)
     except:
-        return print('Type:',type(element),'| Size:', len(element))
-
-'''
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
-'''
+        return print('Type:',type(element),'|| Size:', len(element))
--- test2.py
+++ test2.py
@@ -1,11 +1,8 @@
-# from k_util.main import describe
 from k_util import describe
 import numpy as np
-import pandas as pd
 
 a = [1,2]
 b = np.array([[1,2,0],
               [3,4,9]], copy=True)
 
 describe(b)
-describe(a)
