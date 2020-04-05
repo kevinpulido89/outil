3b4f957103231e1a15208d0ac9f1b20af1e3cba2
Kevin Pulido <kevinp8001@gmail.com>
2020-04-05 13:01:06 -0500

    Añadida la función para concatenar varios archivos .csv

--- k_util.py
+++ k_util.py
@@ -1,5 +1,22 @@
 def describe(element):
+    print()
     try:
         return print('Type:',type(element),'|| Size:', element.shape)
     except:
         return print('Type:',type(element),'|| Size:', len(element))
+    finally:
+        print()
+
+def concatenar_csv(path):
+    from os import listdir
+    import pandas
+
+    files = [file for file in listdir(path) if not file.startswith('.')] # Ignore hidden files
+
+    full_file = pandas.DataFrame()
+
+    for file in files:
+        df = pandas.read_csv(path+"/"+file)
+        full_file = pandas.concat([full_file,df])
+
+    return full_file
