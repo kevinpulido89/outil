49413bdc00654e7ae4512b453137ebfc94ec46f2
Kevin Pulido <kevinp8001@gmail.com>
2020-04-29 00:01:28 -0500

    Minor updates

--- k_util.py
+++ k_util.py
@@ -1,24 +1,25 @@
 def describe(element):
     print()
     t = type(element)
-    
+
     try:
-        return print('Type:',t,'|| Size:', element.shape)
+        return print('Type:', t, '|| Size:', element.shape)
     except:
-        return print('Type:',t,'|| Size:', len(element))
+        return print('Type:', t, '|| Size:', len(element))
     finally:
         print()
+
 
 def concatenar_csv(path):
     from os import listdir
     import pandas
 
-    files = [file for file in listdir(path) if not file.startswith('.')] # Ignore hidden files
+    files = [file for file in listdir(path) if not file.startswith('.')]  # Ignore hidden files
 
     full_file = pandas.DataFrame()
 
     for file in files:
         df = pandas.read_csv(path+"/"+file)
-        full_file = pandas.concat([full_file,df])
+        full_file = pandas.concat([full_file, df])
 
     return full_file
--- test2.py
+++ test2.py
@@ -1,9 +1,8 @@
 from k_util import describe
 import numpy as np
 
-a = [1,2]
-b = np.array([[1,2,0],
-              [3,4,9]], copy=True)
+a = [1, 2]
+b = np.array([[1, 2, 0],
+              [3, 4, 9]], copy=True)
 
-describe(b) #--> Type: <class 'numpy.ndarray'> || Size: (2, 3)
-describe(a)
+describe(b)  # --> Type: <class 'numpy.ndarray'> || Size: (2, 3)
